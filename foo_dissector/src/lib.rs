e92c9a7ce039f7f06050d451196c93b11e3a7e5f
Jose A <jose-pr@coqui.dev>
2020-06-23 01:10:23 +0300

    include dissector function

--- foo_dissector/src/lib.rs
+++ foo_dissector/src/lib.rs
@@ -1,7 +1,18 @@
 use libwireshark::{
-    wireshark_plugin, DissectorAdd, DissectorProtocol, GBool, ModulePref, PrefsValueType,
+    wireshark_plugin, bindings, DissectorAdd, DissectorProtocol, GBool, ModulePref, PrefsValueType,
 };
 use once_cell::sync::Lazy;
+
+
+extern "C" fn dissector(
+    tvb: *mut bindings::tvbuff_t,
+    pinfo: *mut bindings::packet_info,
+    _proto_tree: *mut bindings::proto_tree,
+    _arg4: *mut std::os::raw::c_void,
+) -> std::os::raw::c_int {
+    0
+}
+
 
 static mut FOO_DISSECTOR: Lazy<DissectorProtocol> = Lazy::new(|| DissectorProtocol {
     name: "Foos Protocol",
@@ -14,6 +25,7 @@ static mut FOO_DISSECTOR: Lazy<DissectorProtocol> = Lazy::new(|| DissectorProtoc
         description: "bool_pref Description",
     }],
     dissector_adds: vec![DissectorAdd::Uint("udp.port", 111, true)],
+    dissector,
     ..DissectorProtocol::default()
 });
 wireshark_plugin!(FOO_DISSECTOR);
